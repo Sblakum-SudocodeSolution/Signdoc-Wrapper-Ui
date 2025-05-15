d1c522cad89ad4b9d6fc157e4413f7eefaf831a2
scs-sidhdharth <sblakum@sudocodesolutions.com>
2025-05-15 11:43:31 +0530

    Added form validation and created responsive UI

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,16 +1,53 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  loginObj: any = {
-    email: '',
-    password: '',
-    remember: false,
-  };
+  loginForm!: FormGroup;
+  submitted: boolean = false;
+  private fb = inject(FormBuilder);
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+        ]),
+      ],
+      password: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(16),
+        ]),
+      ],
+      remember: [false, [Validators.requiredTrue]],
+    });
+  }
+
+  get loginFormControls() {
+    return this.loginForm.controls;
+  }
+
+  login() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.loginForm.value);
+    this.loginForm.reset();
+  }
 }
--- src/app/component/signup/signup.component.ts
+++ src/app/component/signup/signup.component.ts
@@ -1,17 +1,53 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
-  signupObj: any = {
-    name: '',
-    email: '',
-    password: '',
-    remember: false,
-  };
+  signupForm!: FormGroup;
+  private fb = inject(FormBuilder);
+
+  ngOnInit() {
+    this.signupForm = this.fb.group({
+      name: ['', Validators.compose([Validators.required])],
+      email: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+        ]),
+      ],
+      password: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(16),
+        ]),
+      ],
+      remember: [false, [Validators.requiredTrue]],
+    });
+  }
+
+  get signupFormControl() {
+    return this.signupForm.controls;
+  }
+
+  signUp() {
+    if (this.signupForm.invalid) {
+      this.signupForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.signupForm.value);
+    this.signupForm.reset();
+  }
 }
